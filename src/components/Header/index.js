b0d2148370fc5679e6d58b318f681a1c6e9c5c02
lorenapassagem <lorenapassagem@gmail.com>
2021-05-20 18:13:29 -0300

    :sparkles: create Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Header = styled.section`
+    background-color: #ffffff;
+    position: absolute;
+    height: 60px;
+    width: 320px;
+`;
+export default Header;
+
