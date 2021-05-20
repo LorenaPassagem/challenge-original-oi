ab2607add088aac68800c8b285c567712fef7941
lorenapassagem <lorenapassagem@gmail.com>
2021-05-20 18:08:38 -0300

    :sparkles: create logo

---
+++ src/components/LogoOriginal/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import Logo from '../../img/logo-original.png'
+
+const LogoOriginal = styled.img.attrs({src: Logo, alt:'logo original'})``
+
+export default LogoOriginal;
