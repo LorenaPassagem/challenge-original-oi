6b482c08965f7a5d114c5db180b3636b063cbbf8
lorenapassagem <lorenapassagem@gmail.com>
2021-05-21 19:32:54 -0300

    :sparkles: add all new components on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
 import Header from './components/Header';
+import IconBag from './components/IconBag';
+import IconSearch from './components/IconSearch';
+import ImgProd from './components/ImgProd';
 import LogoOriginal from './components/LogoOriginal'
+import Menu from './components/Menu';
+import {TitleProduct, Info} from './components/TitleProduct';
+import Base from './styles/settings/Base';
 
 function App() {
 return (
+  <>
    <Header> 
-     <LogoOriginal />
-   </ Header>
+     <Menu />
+     <LogoOriginal />         
+     <IconSearch />
+     <IconBag /> 
+   </Header>
+   <TitleProduct> Rasteira tira dedo</TitleProduct>
+   <Info> RT 0568 | 03.07.0653 </Info>
+   <ImgProd />
+   <Base />
+  </> 
 );
 }
 
