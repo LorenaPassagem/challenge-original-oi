7938439001ba5f7954467ee0f77c79bfe013993d
lorenapassagem <lorenapassagem@gmail.com>
2021-05-21 19:28:51 -0300

    :sparkles: create setting with custom props css

---
+++ src/styles/settings/Base.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Base = createGlobalStyle`
+ #root{
+     --color-gray-dark: #686868;
+     --color-gray-secundary: #7F7F7F;
+     --color-gray-third :#ADADAD;
+     --color-gray-light: #CCCCCC;
+     --color-gray-fourty :#E3E3E3;
+     --color-gray-fifty:#EBEBEB;
+     --color-gray: #F2F2F2;
+
+     --color-primary: #DE8F75;
+ }
+`
+export default Base;
