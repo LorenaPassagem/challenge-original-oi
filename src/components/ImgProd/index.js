cb037547d5b813a332e0b6af49935cd3ff6f4cee
lorenapassagem <lorenapassagem@gmail.com>
2021-05-21 19:26:42 -0300

    :sparkles: add pic produt and create component img Product

---
+++ src/components/ImgProd/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import Photo from '../../img/prod02.png';
+
+const ImgProd = styled.img.attrs({src: Photo, alt: 'foto de uma sandalia'})`
+`;
+export default ImgProd;
