a4224fa87bcfa2838c48b129cfe30aa8f1ad8c01
lorenapassagem <lorenapassagem@gmail.com>
2021-05-21 18:47:24 -0300

    create component Bag in menu

---
+++ src/components/IconBag/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import Bag from '../../img/icon-bag.png';
+
+const IconBag = styled.img.attrs({src: Bag, alt:'icom da sacola de compras'})``;
+
+export default IconBag;
