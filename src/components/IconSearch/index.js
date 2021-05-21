d05ab80301cb9108ddb16a10f589906496404fc9
lorenapassagem <lorenapassagem@gmail.com>
2021-05-21 19:21:40 -0300

    :sparkles: create icon to search

---
+++ src/components/IconSearch/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import Search from '../../img/icon-search.png';
+
+const IconSearch = styled.img.attrs({src: Search, alt: 'icone de icone de pesquisa'})``;
+
+export default IconSearch;
