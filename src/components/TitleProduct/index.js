abed356b9b4a3b6a1da63a2cf8a82fe29f671ff0
lorenapassagem <lorenapassagem@gmail.com>
2021-05-21 19:30:31 -0300

    :sparkles: create components title produts

---
+++ src/components/TitleProduct/index.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const TitleProduct = styled.h1`
+    color: var(--color-primary);
+    font-size: 18px;
+    font-family: Montserrat;
+    text-transform: uppercase;
+    font-weight: normal;
+    text-align: center;
+`;
+
+export const Info = styled.h4`
+    color: var(--color-gray-secundary);
+    font-size: 12px;
+    line-height: 163%;
+    text-align: center;
+`;
