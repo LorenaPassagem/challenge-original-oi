2179d264448d3eb4e875d368729d8d9ec6cb8872
lorenapassagem <lorenapassagem@gmail.com>
2021-05-24 17:34:45 -0300

    create folder objects

---
+++ src/Objects/Cor/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Circles = styled.div`
+    width: 22px;
+    height: 22px;
+    border-radius: 100%;
+    background-color: var(--color-secundary-pink);
+    
+`;
+
+export const LabelColor = styled.div``;
