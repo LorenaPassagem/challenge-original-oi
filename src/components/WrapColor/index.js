8ec9bb6724560f0acff000b74f52bcedc11077e8
lorenapassagem <lorenapassagem@gmail.com>
2021-05-24 18:55:34 -0300

    create a wrap for color  components

---
+++ src/components/WrapColor/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Circles, LabelColor } from '../../Objects/Cor';
+
+   const WrapColor = ({color}) => (
+   <>     
+     <LabelColor>Cor:</LabelColor>
+     <Circles />
+     <Circles />
+     <Circles />
+     <Circles />
+  </>   
+);
+
+export default WrapColor;
+   
