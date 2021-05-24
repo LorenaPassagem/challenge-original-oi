7d1723bf020af3b2d8f941aeaef50b4e069e6144
lorenapassagem <lorenapassagem@gmail.com>
2021-05-24 18:53:32 -0300

    create component SizeProd

---
+++ src/components/SizeProd/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GuideSize, Size, SizeNumber } from './style';
+
+const WrapSize = () => (
+  <>  
+    <Size>Tamanho:</Size>
+    <GuideSize>Guia de Medidas</GuideSize>
+    <SizeNumber />
+  </>  
+)
+
+export default WrapSize;
---
+++ src/components/SizeProd/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Size = styled.section`
+    font-family: Montserrat;
+    font-weight: 400;
+    size:14px;
+`;
+
+export const GuideSize = styled.div`
+    color:var(--color-primary);
+    font-family: Montserrat;
+    float: right;
+    text-decoration:underline;
+`;
+
+export const SizeNumber = styled.div``;
