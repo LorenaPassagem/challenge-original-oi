9de0a527036b167fb729846c1d2c4321411e1512
lorenapassagem <lorenapassagem@gmail.com>
2021-05-27 14:46:44 -0300

    modify components img produt

--- src/components/ImgProd/index.js
+++ src/components/ImgProd/index.js
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 import Photo from '../../img/prod02.png';
 
 const ImgProd = styled.img.attrs({src: Photo, alt: 'foto de uma sandalia'})`
+    margin-left:10%;
 `;
 export default ImgProd;
