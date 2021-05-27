15f6f1467fc37572dea87d36aefad2539a59210a
lorenapassagem <lorenapassagem@gmail.com>
2021-05-27 15:18:26 -0300

    create space between circles

--- src/Objects/Cor/index.js
+++ src/Objects/Cor/index.js
@@ -5,7 +5,9 @@ export const Circles = styled.div`
     height: 22px;
     border-radius: 100%;
     background-color: var(--color-secundary-pink);
-    
+    display: inline-flex;
+    justify-content: space-around;
+    margin: 2px;
 `;
 
 export const LabelColor = styled.div``;
