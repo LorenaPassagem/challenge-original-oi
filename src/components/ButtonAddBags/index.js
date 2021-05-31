b5837fdb83847a643c932e6289224eb9c95f44b1
lorenapassagem <lorenapassagem@gmail.com>
2021-05-31 15:34:56 -0300

    create button adicionar ao sacola

---
+++ src/components/ButtonAddBags/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const ButtonAddBags = styled.button`
+    background-color: var(--color-action);
+    width: 150px;
+    height: 60px;
+    font-family: Montserrat;
+    vertical-align: center;
+    font-size: 18px;
+    color: var(--color-star);
+    border: none;
+    margin-left: 52%;
+`;
+export default ButtonAddBags;
