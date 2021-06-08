68037d8e707885f03638c00d919cb1f68b3d6cba
lorenapassagem <lorenapassagem@gmail.com>
2021-06-08 11:11:31 -0300

    :bento: add color in Basejs

--- src/styles/settings/Base.js
+++ src/styles/settings/Base.js
@@ -6,16 +6,16 @@ const Base = createGlobalStyle`
 
      --color-gray-dark: #686868;
      --color-gray-secundary: #7F7F7F;
-     --color-gray-third :#ADADAD;
+     --color-gray-third: #ADADAD;
      --color-gray-light: #CCCCCC;
-     --color-gray-fourty :#E3E3E3;
-     --color-gray-fifty:#EBEBEB;
+     --color-gray-fourty: #E3E3E3;
+     --color-gray-fifty: #EBEBEB;
+     --color-gray-sixth: #AEB6C1;
      --color-gray: #F2F2F2;
 
      --color-primary: #DE8F75;
      --color-secundary-pink: #A9095E;
      --color-action: #32917B;
-
 
  }
 `
